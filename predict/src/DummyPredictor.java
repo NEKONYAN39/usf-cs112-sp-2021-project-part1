cc1df3ec48b4faed7046d70d21275a04a95eba30
NEKONYAN39 <79959876+NEKONYAN39@users.noreply.github.com>
2021-04-15 11:58:21 +0800

    Update DummyPredictor.java

--- predict/src/DummyPredictor.java
+++ predict/src/DummyPredictor.java
@@ -15,8 +15,8 @@ public class DummyPredictor extends Predictor {
         BufferedReader bufferedReader = null;
         ArrayList<DataPoint> dataPoints = new ArrayList<>();
         try {
-            inputStream = new FileInputStream(filename);// 文件输入流
-            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));// 缓冲输入流
+            inputStream = new FileInputStream(filename);
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
             String line = null;
             while ((line = bufferedReader.readLine())!=null){
                 String[] s = line.split(" ");
